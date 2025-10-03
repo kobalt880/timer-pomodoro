9fbac5ef4e97a6b1f4548e2e18b3b3fdb58a9413
Kobalt880 <kobalt8800@gmail.com>
2025-10-03 18:19:52 +0300

    Добавлен класс TimeFrame

--- classes.py
+++ classes.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from threading import Thread
 from time import sleep
+from pygame import mixer
+mixer.init()
+sound = mixer.Sound('sound.wav')
 
 
 class Timer:
@@ -41,19 +44,18 @@ class Timer:
         return self._seconds
 
     def __str__(self):
-        h = self._seconds // 60 // 60
-        m = self._seconds // 60 - h * 60
-        s = self._seconds - m * 60 - h * 3600
+        m = self._seconds // 60
+        s = self._seconds - m * 60
 
-        return (f'{"0" if h < 10 else ""}{h}:'
-                f'{"0" if m < 10 else ""}{m}:'
+        return (f'{"0" if m < 10 else ""}{m}:'
                 f'{"0" if s < 10 else ""}{s}')
 
 
 class PomodoroCycle:
     def __init__(self, work_tick_func, break_tick_func, work_end_func,
                  break_end_func, cycle_end_func, break_time: int = 300,
-                 long_break_time: int = 1200, work_time: int = 1500, iter_count: int = 4):
+                 long_break_time: int = 1200, work_time: int = 1500,
+                 iter_count: int = 4):
         self._break_time = break_time
         self._long_break_time = long_break_time
         self._work_time = work_time
@@ -119,4 +121,81 @@ class PomodoroCycle:
         return str(self._timer)
         
 
+class TimeFrame(Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.columnconfigure([0, 1, 2], weight=1)
+        self.rowconfigure([0, 1], weight=1)
+        self._create_widgets()
+        
+        self._pc = None
+        self._cycle_count = 0
+        
+        self._set_status('-')
+        self._increase_cycle_count()
 
+    def _create_widgets(self):
+        self._status_label = Label(self)
+        self._cycle_count_label = Label(self)
+        self._time_label = Label(self, text='00:00', font=('arial', 30))
+        launch_button = Button(self, text='Запустить', command=self.launch)
+        reset_button = Button(self, text='Сбросить', command=self.reset)
+        stop_button = Button(self, text='Остановить', command=self.stop)
+
+        self._status_label.grid(column=0, row=0, sticky=W)
+        self._cycle_count_label.grid(column=0, row=1, sticky=W)
+        self._time_label.grid(column=0, row=2, padx=20, pady=20, columnspan=3)
+        launch_button.grid(column=0, row=3, pady=10, padx=5)
+        reset_button.grid(column=1, row=3, pady=10, padx=5)
+        stop_button.grid(column=2, row=3, pady=10, padx=5)
+
+    def launch(self):
+        if self._pc is None:
+            self._pc = PomodoroCycle(self._upd_time, self._upd_time,
+            self._work_end, self._break_end, self.launch,
+            30, 160, 60)
+            self._pc.launch()
+            self._set_status('Работа')
+        else:
+            self._pc.launch()
+
+    def reset(self):
+        self._pc.stop()
+        self._pc = None
+        self._set_time('00:00')
+        self._set_status('-')
+        self._cycle_count = 0
+        self._increase_cycle_count()
+
+    def stop(self):
+        if self._pc is not None:
+            self._pc.stop()
+
+    def _work_end(self):
+        sound.play()
+        self._set_status('Перерыв')
+
+    def _break_end(self):
+        sound.play()
+        self._increase_cycle_count()
+        self._set_status('Работа')
+
+    def _upd_time(self):
+        text = str(self._pc.get_timer())
+        self._set_time(text)
+
+    def _set_time(self, time: str):
+        self._time_label.configure(text=time)
+
+    def _set_status(self, status: str):
+        self._status_label.configure(text=f'Статус: {status}')
+
+    def _increase_cycle_count(self):
+        self._cycle_count += 1
+        text = 'Помодоро № ' + str(self._cycle_count)
+        self._cycle_count_label.configure(text=text)
+
+
+
+
+    
--- main.pyw
+++ main.pyw
@@ -2,30 +2,12 @@ from classes import *
 
 
 def main():
-    during = True
+    win = Tk()
 
-    def cycle_end():
-        nonlocal during
-        during = False
-        print('END')
-
-    def work_end():
-        print('work end')
-
-    def break_end():
-        print('break end')
-
-    def tick():
-        print(pc)
-
-    pc = PomodoroCycle(tick, tick, work_end, break_end, cycle_end, 15, 5, 20)
-    pc.launch()
+    tf = TimeFrame(win)
+    tf.pack()
     
-    while during:
-        sleep(5)
-        pc.launch()
-        sleep(5)
-        pc.stop()
+    win.mainloop()
         
 
 if __name__ == '__main__':
